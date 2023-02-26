33490c9e03cb167bfb074f5d0a3f1e9593bc1bd4
Matheus Inácio <29243277+matheus-inacio@users.noreply.github.com>
2023-02-26 16:23:56 -0300

    Update androidHookScript.js

--- scripts/androidHookScript.js
+++ scripts/androidHookScript.js
@@ -9,7 +9,7 @@ module.exports = function(ctx) {
     
     /* Updates splash icon */
     var icSplashPath = path.join(ctx.opts.projectRoot, 'platforms/android/app/src/main/res/values/themes.xml');
-    var themesDoc = xmlHelpers.parseElementtreeSync(manifestPath);
+    var themesDoc = xmlHelpers.parseElementtreeSync(icSplashPath);
     var iconTag = doc.find('item[@name="windowSplashScreenAnimatedIcon"]');
     iconTag.text = "@drawable/eg_logo";
     fs.writeFileSync(icSplashPath, themesDoc.write({indent: 4}), 'utf-8');
