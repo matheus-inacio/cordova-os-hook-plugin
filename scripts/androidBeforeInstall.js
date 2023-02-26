e3fe1cf60fa2de45434202d476cb6c49ff48c22b
Matheus Inácio <29243277+matheus-inacio@users.noreply.github.com>
2023-02-26 01:16:02 -0300

    Update androidBeforeInstall.js

--- scripts/androidBeforeInstall.js
+++ scripts/androidBeforeInstall.js
@@ -5,17 +5,16 @@ const { events, xmlHelpers } = require('cordova-common');
 
 
 module.exports = function(ctx) {
+    var manifestPath = path.join(ctx.opts.projectRoot, 'platforms/android/app/src/main/AndroidManifest.xml');
+    
     try {
-      var stylePath = path.join(ctx.opts.projectRoot, 'platforms/android/app/src/main/res/values/styles.xml');
-      const data = fs.readFileSync(stylePath, 'utf8');
+      const data = fs.readFileSync(manifestPath, 'utf8');
       console.log(data);
       events.emit('log', data)
     } catch (err) {
       console.error(err);
     }
-    
-    
-    var manifestPath = path.join(ctx.opts.projectRoot, 'platforms/android/app/src/main/AndroidManifest.xml');
+
     var doc = xmlHelpers.parseElementtreeSync(manifestPath);
     if (doc.getroot().tag !== 'manifest') {
         throw new Error(manifestPath + ' has incorrect root node name (expected "manifest")');
