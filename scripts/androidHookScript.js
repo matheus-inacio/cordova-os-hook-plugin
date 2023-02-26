0a1ed35c6ec37fcb3ce9465f4ff1a9a394ce5012
Matheus Inácio <29243277+matheus-inacio@users.noreply.github.com>
2023-02-26 18:44:33 -0300

    Update androidHookScript.js

--- scripts/androidHookScript.js
+++ scripts/androidHookScript.js
@@ -11,18 +11,5 @@ module.exports = function(ctx) {
     var icSplashPath = path.join(ctx.opts.projectRoot, 'platforms/android/app/src/main/res/values/themes.xml');
     var data = fs.readFileSync(icSplashPath, 'utf8');
     var result = data.replace(/ic_cdv_splashscreen/g, 'splash_inset');
-    console.log(data.toString());
     fs.writeFileSync(icSplashPath, result, 'utf-8');
-
-    return;
-    
-    /* Updates the theme in the android manifest */
-    var manifestPath = path.join(ctx.opts.projectRoot, 'platforms/android/app/src/main/AndroidManifest.xml');
-    var doc = xmlHelpers.parseElementtreeSync(manifestPath);
-    if (doc.getroot().tag !== 'manifest') {
-        throw new Error(manifestPath + ' has incorrect root node name (expected "manifest")');
-    }
-    var activityTag = doc.find("application/activity");
-    activityTag.attrib["android:theme"] = "@style/AppTheme";
-    fs.writeFileSync(manifestPath, doc.write({indent: 4}), 'utf-8');
 };
