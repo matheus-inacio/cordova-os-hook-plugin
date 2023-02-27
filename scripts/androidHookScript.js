69d6179fb113d7c2f45cd288adfa758f84d7cc59
Matheus Inácio <29243277+matheus-inacio@users.noreply.github.com>
2023-02-26 22:11:53 -0300

    Update androidHookScript.js

--- scripts/androidHookScript.js
+++ scripts/androidHookScript.js
@@ -12,4 +12,16 @@ module.exports = function(ctx) {
     var data = fs.readFileSync(icSplashPath, 'utf8');
     var result = data.replace(/ic_cdv_splashscreen/g, 'splash_inset');
     fs.writeFileSync(icSplashPath, result, 'utf-8');
+
+    /* Updates splash icon */
+    var cordovaActivityPath = path.join(ctx.opts.projectRoot, 'platforms/android/app/org/apache/cordova/CordovaActivity.java');
+    var activityData = fs.readFileSync(cordovaActivityPath, 'utf8');
+    var statusBarCode = `
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().setStatusBarColor(ContextCompat.getColor(activity,R.color.cdv_splashscreen_background));
+        super.onCreate(savedInstanceState);`;
+    var newCode = activityData.replace(/super.onCreate(savedInstanceState);/g, statusBarCode);
+    console.log(newCode.toString());
+    fs.writeFileSync(cordovaActivityPath, newCode, 'utf-8');
 };
